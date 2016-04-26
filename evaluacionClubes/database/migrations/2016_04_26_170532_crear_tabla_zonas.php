931995451b0e4a2e908688925e7758630fe8f658
nfredes <niko.afv@gmail.com>
2016-04-26 14:22:48 -0300

    Se agregan migraciones para algunas tablas (users, campos, zonas, cargos, rangos y perfiles)

--- evaluacionClubes/database/migrations/2016_04_26_170532_crear_tabla_zonas.php
+++ evaluacionClubes/database/migrations/2016_04_26_170532_crear_tabla_zonas.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CrearTablaZonas extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('zonas', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('profile')->unsigned()->default(1);
-            $table->rememberToken();
+            $table->string('nombre',255);
+            $table->integer('campo')->unsigned()->default(1);
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('zonas');
     }
 }
--- evaluacionClubes/database/migrations/2016_04_26_170720_crear_tabla_cargos.php
+++ evaluacionClubes/database/migrations/2016_04_26_170720_crear_tabla_cargos.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CrearTablaCargos extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cargos', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('profile')->unsigned()->default(1);
-            $table->rememberToken();
+            $table->string('nombre',255);
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('cargos');
     }
 }
