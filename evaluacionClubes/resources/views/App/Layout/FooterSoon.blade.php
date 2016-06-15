19f7422215e2f88700007566d702e951293982bf
nfredes <niko.afv@gmail.com>
2016-06-15 16:46:38 -0300

    Se corrige header de coming soon y logo de footer

--- evaluacionClubes/resources/views/App/Layout/FooterSoon.blade.php
+++ evaluacionClubes/resources/views/App/Layout/FooterSoon.blade.php
@@ -3,7 +3,7 @@
         <div class="rights">
             <!--<h1>Conquis<span>AMCH</span></h1> &#169; <span id="copyright-year"></span>&nbsp;-->
 
-            <img src="/assets/app/images/conquis/logo_amch.png" />
+            <img src="/assets/app/images/conquis/logo_amch.png" width="150" />
             <a href="https://www.facebook.com/RegionalAMCH"><i class="fa fa-facebook-square"></i>&nbsp; Visitanos en Facebook</a>
         </div>
     </div>
--- evaluacionClubes/resources/views/App/Layout/HeaderSoon.blade.php
+++ evaluacionClubes/resources/views/App/Layout/HeaderSoon.blade.php
@@ -1,3 +1,22 @@
+<div id="stuck_container" class="stuck_container">
+    <nav class="navbar navbar-default navbar-static-top ">
+        <div class="container">
+            <div class="navbar-header">
+                <h1 class="navbar-brand">
+                    <!--<a data-type='rd-navbar-brand' href="./">Constr<span>ex</span></a>-->
+                    <img src="/assets/app/images/conquis/logo_amch.png" />
+                </h1>
+            </div>
+
+            <ul class="navbar-nav sf-menu navbar-right" data-type="navbar">
+                <li class="dropdown">
+                    <a href="./">Pronto</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
+
 <!--  -->
 <section class="well well-15">
     <div class="container text-center">
