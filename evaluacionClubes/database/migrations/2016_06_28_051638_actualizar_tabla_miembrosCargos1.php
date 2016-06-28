bc5ac88727fd1bd22cc3ff2d6d1e38027edd0af4
Nicolás Fredes <niko.afv@gmail.com>
2016-06-28 03:30:46 -0300

    Se renombrn

--- evaluacionClubes/database/migrations/2016_06_28_051638_actualizar_tabla_miembrosCargos1.php
+++ evaluacionClubes/database/migrations/2016_06_28_051638_actualizar_tabla_miembrosCargos1.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ActualizarTablaMiembrosCargos extends Migration
+class ActualizarTablaMiembrosCargos1 extends Migration
 {
     /**
      * Run the migrations.
